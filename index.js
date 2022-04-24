fbf05d42dc1ecd1a71474552e2b54da1742a764f
ooooorobo <orobos654@gmail.com>
2022-04-24 19:28:23 +0900

    동적 데이터 렌더링 상황 가정

--- index.js
+++ index.js
@@ -22,9 +22,23 @@ const state = {
  * - 다음 리페인트가 이벤트 루프에서 스케줄링되기 직전에 실행된다.
  * [브라우저 렌더링(HTML 로드) -> 다음 렌더링 대기 -> 새 가상 노드(view.js) -> DOM 조작(index.js) -> 브라우저 렌더링]
  */
-window.requestAnimationFrame(() => {
-    // 최초 DOM
-    const main = document.querySelector('.todoapp')
-    const newMain = registry.renderRoot(main, state)
-    main.replaceWith(newMain)
-})
+const render = () => {
+    window.requestAnimationFrame(() => {
+        // 최초 DOM
+        const main = document.querySelector('.todoapp')
+        const newMain = registry.renderRoot(main, state)
+        main.replaceWith(newMain)
+    })
+}
+
+/**
+ * 5초마다 데이터가 변경되는 동적 데이터라고 가정하고 렌더링힌다
+ * --> 데이터가 변경될 때마다 가상 루트 요소를 만든 다음 실제 요소를 대체한다
+ * --> 프로젝트가 커질수록 새로 그려야 하는 요소의 양이 많아진다 --> 성능 저하 가능성 발생
+ */
+window.setInterval(() => {
+    state.todos = getTodos()
+    render()
+}, 5000)
+
+render()
