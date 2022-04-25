a3dc1539703e5f8a2709c4e545bf5bccf63b7ac5
ooooorobo <orobos654@gmail.com>
2022-04-25 22:19:24 +0900

    라우팅 3. 경로 파라미터 사용하기 - 정규식 이용한 파싱

--- router-demo/pages.js
+++ router-demo/pages.js
@@ -7,10 +7,21 @@ export default container => {
         container.textContent = '리스트 페이지입니다'
     }
 
+    // 매개변수 읽기 기능 추가
+    const detail = (params) => {
+        const {id} = params
+        container.textContent = `${id}번 페이지입니다`
+    }
+
+    const moreDetail = (params) => {
+        const {id, anotherId} = params
+        container.textContent = `이 페이지는 ${id}번 페이지의 ${anotherId}번 페이지입니다`
+    }
+
     const notFound = () => {
         container.textContent = '페이지를 찾을 수 없습니다'
     }
 
-    return {home, list, notFound}
+    return {home, list, detail, moreDetail, notFound}
 }
 
--- router-demo/router.js
+++ router-demo/router.js
@@ -1,25 +1,57 @@
 export default () => {
+    const ROUTE_PARAMETER_REGEXP = /:(\w+)/g
+    const URL_FRAGMENT_REGEXP = '([^\\/]+)'
+
     const routes = []
     let notFound = () => {}
 
     const router = {}
-    const checkRoutes = () => {
-        const currentRoute = routes.find(route => {
-            return route.fragment === window.location.hash
+
+    // fragment마다 그려줄 컴포넌트를 등록
+    router.addRoute = (fragment, component) => {
+        const params = []
+        const parsedFragment = fragment
+            .replace(ROUTE_PARAMETER_REGEXP, (match, paramName) => {
+                params.push(paramName)
+                return URL_FRAGMENT_REGEXP
+            })
+            .replace(/\//g, '\\/')
+
+        routes.push({testRegExp: new RegExp(`^${parsedFragment}$`), component, params})
+        return router
+    }
+
+    const extractUrlParams = (route, windowHash) => {
+        if (route.params.length === 0) {
+            return {}
+        }
+
+        const params = {}
+
+        const matches = windowHash.match(route.testRegExp)
+        matches.shift()
+        matches.forEach((paramValue, index) => {
+            const paramName = route.params[index]
+            params[paramName] = paramValue
         })
 
+        return params
+    }
+
+    const checkRoutes = () => {
+        const {hash} = window.location
+
+        const currentRoute = routes.find(route => {
+            const {testRegExp} = route
+            return testRegExp.test(hash)
+        })
         if (!currentRoute) {
             notFound()
             return
         }
-
-        currentRoute.component()
-    }
-
-    // fragment마다 그려줄 컴포넌트를 등록
-    router.addRoute = (fragment, component) => {
-        routes.push({fragment, component})
-        return router
+        const urlParams = extractUrlParams(currentRoute, window.location.hash)
+        // 파라미터를 포함해서 컴포넌트를 렌더링하도록 함
+        currentRoute.component(urlParams)
     }
 
     // 알맞은 route 찾지 못할 경우 보여줄 페이지 방식 등록
